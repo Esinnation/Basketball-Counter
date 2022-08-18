0a8c20c7fed6eb04db86ca8af8d640d5e64492fb
Esinnation <abdulrazaqridollah96@gmail.com>
2022-08-18 01:58:14 -0700

    highlight the leader

--- script.js
+++ script.js
@@ -7,66 +7,66 @@ const plus3= document.getElementById('plus3')
 const score1=document.getElementById('score1')
 const score2=document.getElementById('score2')
 const newGame=document.getElementById('new-btn')
-// const home=document.getElementById('home')
-// const guest=document.getElementById('guest')
+const home=document.getElementById('home')
+const guest=document.getElementById('guest')
 let count1 =0
 let count2 =0
 let total=0
 
-// function winning(){
-//     if(count1>0||count2>0){
-//         if (count1>count2){
-//             home.className='win'
-//             guest.className=''
-//         }else if (count2 >count1){
-//             guest.className='win'
-//             home.className=''
-//         }else{
-//             guest.className=''
-//             home.className=''
-//         }
-//     }
-// }
+function winning(){
+    if(count1>0||count2>0){
+        if (count1>count2){
+            home.className='win'
+            guest.className=''
+        }else if (count2 >count1){
+            guest.className='win'
+            home.className=''
+        }else{
+            guest.className=''
+            home.className=''
+        }
+    }
+}
 
 
     plusOne.addEventListener('click',function(){
         count1++
         let total=count1
         score1.textContent=count1
-        // winning()
+        winning()
     })
 
     plusTwo.addEventListener('click',function(){
         count1+=2
         let total=count1
         score1.textContent=count1
-        // winning()
+        winning()
     })
     
     plusThree.addEventListener('click',function(){
         count1+=3
         let total=count1
         score1.textContent=count1
-        // winning()
+        winning()
     })
 
 
     plus1.addEventListener('click',function(){
         count2++
         score2.textContent=count2
-        // winning()
+        winning()
     })
     
     plus2.addEventListener('click',function(){
         count2+=2
         score2.textContent=count2
-        // winning()
+        winning()
     })
     
     plus3.addEventListener('click',function(){
         count2+=3
         score2.textContent=count2
-        // winning()
+        winning()
     })
 
     
